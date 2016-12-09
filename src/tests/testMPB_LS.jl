a8f766e084aa4853058da94a000c36bb58c7dd40
JPD <vepiteski@gmail.com>
2016-12-09 14:09:20 -0500

    minor changes

--- src/tests/testMPB_LS.jl
+++ src/tests/testMPB_LS.jl
@@ -22,7 +22,7 @@ probs = filter(name -> name != :OptimizationProblems
 
 
 mpb_probs = (MathProgNLPModel(eval(p)(n),  name=string(p) )   for p in probs)
-tst_prob = MathProgNLPModel(eval(:brownbs)(n),  name=string(:brownbs) );
+tst_prob = MathProgNLPModel(eval(:woods)(n),  name=string(:woods) );
 
 
 
@@ -33,19 +33,20 @@ using LSDescentMethods
 using LineSearch
 #solver = Newlbfgs
 #solver = steepest
-solver = CG_PR
-#solver(tst_prob,linesearch=TR_Sec_ls)
+#solver = Newton
+#(x, f, ∇fNorm, iter, optimal, tired, status)=solver(tst_prob)
 
 include("../compare_solvers.jl")
 
-solvers = [steepest,Newlbfgs,CG_PR,LbfgsB]
+solvers = [steepest,Newlbfgs,Newton, CG_PR]
 
-labels = ["steepestSimple0.2-0.6", "lbfgsSimple","CG_PRTR_Sec0.45-0.56","LbfgsB"]
+labels = ["steepestSimple0.2-0.6", "lbfgsSimple","Newton","CG"]
 
 options = [Dict{Symbol,Any}(:τ₀=>0.2, :τ₁=>0.6) 
-           Dict{Symbol,Any}(:linesearch => TR_SecA_ls, :τ₀=>0.45, :τ₁=>0.56) 
-           Dict{Symbol,Any}() ] # The basic greedy line search
+           Dict{Symbol,Any}() 
+           Dict{Symbol,Any}() 
+           Dict{Symbol,Any}(:linesearch=>TR_Sec_ls, :τ₀=>0.4, :τ₁=>0.6)  ] # The basic greedy line search
 
-#s1, P1 = compare_solvers_with_options(solvers, options, labels, mpb_probs, n_min, n_max)
+s1, P1 = compare_solvers_with_options(solvers, options, labels, mpb_probs, n_min, n_max)
 
-s1, P1 = compare_solver_options(solver, options, mpb_probs, n_min, n_max)
+#s1, P1 = compare_solver_options(solver, options, mpb_probs, n_min, n_max)
